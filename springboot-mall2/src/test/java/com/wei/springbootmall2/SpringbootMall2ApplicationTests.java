573ad8ee4a3169eab1de7d79b5ae01db80c1fbef
Zengwei0321 <Alittlechance0321@gmail.com>
2023-06-08 13:27:35 +0800

    初始化 Spring boot

---
+++ springboot-mall2/src/test/java/com/wei/springbootmall2/SpringbootMall2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.springbootmall2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMall2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
